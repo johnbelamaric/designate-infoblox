aa1a981a4666c133bde1f2645afd057785d5c6fb
John Belamaric <jbelamaric@infoblox.com>
2015-05-06 13:40:44 -0700

    Clean up unit tests

--- designate_infoblox/tests/test_impl_infoblox.py
+++ designate_infoblox/tests/test_impl_infoblox.py
@@ -12,70 +12,21 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-import collections
-
 from mock import MagicMock
 
 from designate import objects
 from designate.tests.test_backend import BackendTestCase
-from designate import utils
-from designate_infoblox.impl_infoblox import exceptions
 from designate_infoblox.impl_infoblox import InfobloxBackend
 
 
 class InfobloxBackendTestCase(BackendTestCase):
-    def create_record_primitive(self, ip):
-        def to_primitive():
-            return {
-                'designate_object.data': {
-                    'data': ip,
-                    'name': 'some.example.com'
-                }
-            }
-        return to_primitive
 
     def get_domain_fixture(self):
         return super(InfobloxBackendTestCase, self).get_domain_fixture(
             values={
-                'name': 'test.example.com'
+                'name': 'test.example.com.'
             }
         )
-
-    def get_recordset_fixture(self, domain_name, record_type):
-        def obj_get_changes():
-            return {
-                'records': [
-                    collections.namedtuple(
-                        'RecordSet', {'data': '172.25.1.1'}
-                    )(data='172.25.1.1')
-                ]
-            }
-
-        values = super(InfobloxBackendTestCase, self).get_recordset_fixture(
-            domain_name=domain_name,
-            values={
-                'id': utils.generate_uuid(),
-                'ttl': 123456,
-                'to_primitive': self.create_record_primitive('172.25.1.2'),
-                'obj_get_changes': obj_get_changes
-            }
-        )
-        return collections.namedtuple('RecordSet', values)(**values)
-
-    def get_record_fixture(self, recordset_type):
-
-        values = super(InfobloxBackendTestCase, self).get_record_fixture(
-            recordset_type,
-            values={
-                'id': utils.generate_uuid(),
-                'to_primitive': self.create_record_primitive('172.25.1.1')
-            }
-        )
-        return collections.namedtuple('Record', values)(**values)
-
-    def get_server_fixture(self):
-        values = super(InfobloxBackendTestCase, self).get_server_fixture()
-        return collections.namedtuple('Server', values)(**values)
 
     def setUp(self):
         super(InfobloxBackendTestCase, self).setUp()
@@ -90,87 +41,24 @@ class InfobloxBackendTestCase(BackendTestCase):
         self.backend.start()
         self.backend.infoblox = MagicMock()
 
-    def test_create_server(self):
-        context = self.get_context()
-        server = self.get_server_fixture()
-        self.backend.create_server(context, server)
-
-    def test_create_server_fail(self):
-        self.backend.infoblox.get_member = MagicMock(
-            return_value=[])
-
-        context = self.get_context()
-        server = self.get_server_fixture()
-        self.assertRaises(exceptions.NoInfobloxMemberAvailable,
-                          self.backend.create_server, context, server)
-
-    def test_update_server(self):
-        context = self.get_context()
-        server = self.get_server_fixture()
-        self.backend.create_server(context, server)
-        self.backend.update_server(context, server)
-
-    def test_update_server_fail(self):
-        context = self.get_context()
-        server = self.get_server_fixture()
-        self.backend.create_server(context, server)
-
-        self.backend.infoblox.get_member = MagicMock(
-            return_value=[])
-        self.assertRaises(exceptions.NoInfobloxMemberAvailable,
-                          self.backend.update_server, context, server)
-
-    def test_delete_server(self):
-        context = self.get_context()
-        server = self.get_server_fixture()
-        self.backend.create_server(context, server)
-        self.backend.delete_server(context, server)
-
     def test_create_domain(self):
         context = self.get_context()
-        server = self.get_server_fixture()
         domain = self.get_domain_fixture()
-        self.backend.create_server(context, server)
+        self.backend.infoblox.get_dns_view = MagicMock(return_value='default')
         self.backend.create_domain(context, domain)
+        self.backend.infoblox.create_zone_auth.assert_called_once_with(
+                              fqdn='test.example.com',
+                              dns_view='default')
 
     def test_update_domain(self):
         context = self.get_context()
-        server = self.get_server_fixture()
         domain = self.get_domain_fixture()
-        self.backend.create_server(context, server)
-        self.backend.create_domain(context, domain)
         self.backend.update_domain(context, domain)
 
     def test_delete_domain(self):
         context = self.get_context()
-        server = self.get_server_fixture()
         domain = self.get_domain_fixture()
-        self.backend.create_server(context, server)
         self.backend.create_domain(context, domain)
         self.backend.delete_domain(context, domain)
-
-    def test_create_record(self):
-        context = self.get_context()
-        server = self.get_server_fixture()
-        domain = self.get_domain_fixture()
-
-        recordset = self.get_recordset_fixture(domain['name'], "A")
-        record = self.get_record_fixture("A")
-
-        self.backend.create_server(context, server)
-        self.backend.create_domain(context, domain)
-        self.backend.create_record(context, domain, recordset, record)
-
-    def test_delete_record(self):
-        context = self.get_context()
-        server = self.get_server_fixture()
-        domain = self.get_domain_fixture()
-
-        recordset = self.get_recordset_fixture(domain['name'], "A")
-        record = self.get_record_fixture("A")
-
-        self.backend.create_server(context, server)
-        self.backend.create_domain(context, domain)
-        self.backend.create_record(context, domain, recordset, record)
-
-        self.backend.delete_record(context, domain, recordset, record)
+        self.backend.infoblox.delete_zone_auth.assert_called_once_with(
+                              'test.example.com')
